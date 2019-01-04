9ccae40baa0d92d58a62794dca267997b74040b7
MihailKolovretov <frontber@bk.ru>
2019-01-04 22:20:29 +0300

    lesson - 6: добавлен список+ список точек

--- snakeCsharp/snakeCsharp/Program.cs
+++ snakeCsharp/snakeCsharp/Program.cs
@@ -8,15 +8,43 @@ namespace snakeCsharp
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,"#");
-            p1.Draw();
+           Point p1 = new Point(1, 3, "#");
+           p1.Draw();
 
-            Point p2 = new Point(1, 5, "*");
-            p2.Draw();
+           Point p2 = new Point(1, 5, "*");
+           p2.Draw();
 
+            Point p3 = new Point(8, 8, "$");
+            p3.Draw();
+
+            Point p4 = new Point(10, 15, "@");
+            p4.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add (0);
+            numList.Add (1);
+            numList.Add (2);
+
+
+            foreach (int i in numList) {
+                Console.WriteLine(i);
+            }
+
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+
+            foreach (Point i in pList)
+            {
+                Console.WriteLine(i);
+            }
 
             Console.ReadLine();
         }
