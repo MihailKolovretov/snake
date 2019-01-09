a63854703c629f4906eba1b46cc6b76fd224c8a3
MihailKolovretov <frontber@bk.ru>
2019-01-09 22:43:08 +0300

    докомитил изменения 8 урока

---
+++ snakeCsharp/snakeCsharp/Figura.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Все общие куски кода суем в родительский класс они становятся доступны в дочерних классах
+ * 
+ * 
+ * 
+ * */
+namespace snakeCsharp
+{
+    class Figura// класс фигура(родительский)
+    {
+       protected List<Point> pList;// одинаковые куски кода суем в родительский класс
+
+        public void Draw()// выводим список точек
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+
+    }
+}
---
+++ snakeCsharp/snakeCsharp/HorisontalLines.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeCsharp
+{
+    class HorisontalLines : Figura
+    {
+        
+
+        public HorisontalLines(int xLeft, int xRight, int y, string simb)
+        {
+            pList = new List<Point>(); //создаем список точек от левого х до правого х
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, simb);
+                pList.Add(p);
+            }
+        }
+      
+    }
+}
